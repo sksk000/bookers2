f330d6e69370d2641e5113a3f23c5b7e1a569f5f
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-05-20 13:24:29 +0000

    [add]今回必要なroutesの設定を行った

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
   devise_for :users
+  root to: 'homes#top'
+
+  resources :books, only[:new, :index, :show, :create, :destroy]
+
+  resources :users, only[:show, :index, :edit, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
