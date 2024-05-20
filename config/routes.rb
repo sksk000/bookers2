abc5f0914667156fe2daeb5ab216d592189fc36a
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-05-20 14:31:36 +0000

    [update]アソシエーションの設定を行った

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :books, only[:new, :index, :show, :create, :destroy]
 
   resources :users, only[:show, :index, :edit, :update]
+  
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
