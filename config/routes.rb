ed4239f466d5cb9f5aedcbab48836f98add9702a
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-05-22 13:12:18 +0000

    [update]signup loginにてform forを使っている部分をform withに変更、topに記述されていた紹介文削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :books, only[:new, :index, :show, :create, :destroy]
+  resources :books, only:[:new, :index, :show, :create, :destroy]
 
-  resources :users, only[:show, :index, :edit, :update]
-  
-  
+  resources :users, only:[:show, :index, :edit, :update]
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
