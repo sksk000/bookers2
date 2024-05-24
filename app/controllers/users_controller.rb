cbdecc7297661a7e9f7243099c250e84b71ce686
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-05-24 14:48:06 +0000

    [update]UserのEdit処理、Update処理実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
-  before_action :is_matching_login_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image)
   end
 end
