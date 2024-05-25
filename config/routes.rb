6ee7fdf7218e2ad5e6ba124bb05f549000cd60fc
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-05-25 08:19:31 +0000

    [update]bookのedit・show処理実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
 
-  resources :books, only:[:new, :index, :show, :create, :destroy]
+  resources :books, only:[:new, :index, :show, :create, :destroy, :edit, :update]
   resources :users, only:[:show, :index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
